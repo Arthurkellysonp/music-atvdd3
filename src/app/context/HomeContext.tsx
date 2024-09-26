325312f880c3a8dfbdc22e382c726fe9c16188e5
Arthurkellysonp <arthur.negreiros@alunos.ufersa.edu.br>
2024-09-26 17:34:13 -0300

    first commit

--- src/app/context/HomeContext.tsx
+++ src/app/context/HomeContext.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import React, {createContext, ReactNode, useEffect, useState} from 'react';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
 
 type HomeContextData = {
     playing: boolean;
@@ -12,8 +12,8 @@ type HomeContextData = {
     muted: boolean;
     configPlayPause: () => void;
     configAudio: () => void;
-    configAudioIndex: (index:number) => void;
-    configVolume: (valeu: number) => void;
+    configAudioIndex: (index: number) => void;
+    configVolume: (value: number) => void;
     configPanner: (value: number) => void;
     configCurrentTime: (value: number) => void;
     configMuted: () => void;
@@ -25,7 +25,7 @@ type ProviderProps = {
     children: ReactNode;
 }
 
-const HomeContextProvider = ({children}:ProviderProps) => {
+const HomeContextProvider = ({ children }: ProviderProps) => {
     const [playing, setPlaying] = useState(false);
     const [volume, setVolume] = useState(1);
     const [currentTime, setCurrentTime] = useState(0);
@@ -33,11 +33,11 @@ const HomeContextProvider = ({children}:ProviderProps) => {
     const [totalTime, setTotalTime] = useState(0);
     const [panner, setPanner] = useState(0);
     const [audioIndex, setAudioIndex] = useState(0);
-    const [audio, setAudio] = useState<HTMLAudioElement>();
-    const [gain, setGain] = useState<GainNode>(); 
-    const [stereo, setStereo] = useState<StereoPannerNode>();
+    const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+    const [gain, setGain] = useState<GainNode | null>(null);
+    const [stereo, setStereo] = useState<StereoPannerNode | null>(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         if (audio) {
             if (playing) {
                 play();
@@ -45,17 +45,17 @@ const HomeContextProvider = ({children}:ProviderProps) => {
 
             audio.onloadedmetadata = () => {
                 setTotalTime(audio.duration);
-            }
+            };
 
             audio.ontimeupdate = () => {
                 setCurrentTime(audio.currentTime);
-            }
+            };
 
             audio.onended = () => {
                 configAudioIndex(audioIndex + 1);
-            }
+            };
         }
-    }, [audio]);
+    }, [audio, playing]);
 
     const configCurrentTime = (value: number) => {
         if (!audio) return;
@@ -68,86 +68,88 @@ const HomeContextProvider = ({children}:ProviderProps) => {
     }
 
     const configAudioIndex = (index: number) => {
-        const newAudioIndex = index % 3;
-        alert(newAudioIndex);
+        const newAudioIndex = index % 3; // Ajustar conforme o número de músicas
         const updatedAudio = new Audio(`audios/audio${newAudioIndex + 1}.mp3`);
         pause();
         setAudioIndex(newAudioIndex);
         setAudio(updatedAudio);
-        const audioConext = new AudioContext();
-        const media = audioConext.createMediaElementSource(updatedAudio);
-        const updatedGain = audioConext.createGain();
-        const updatedStereo = audioConext.createStereoPanner();
+
+        const audioContext = new AudioContext();
+        const media = audioContext.createMediaElementSource(updatedAudio);
+        const updatedGain = audioContext.createGain();
+        const updatedStereo = audioContext.createStereoPanner();
         media.connect(updatedGain);
         updatedGain.connect(updatedStereo);
-        updatedStereo.connect(audioConext.destination);
-        
+        updatedStereo.connect(audioContext.destination);
+
         updatedAudio.onplay = () => {
-            audioConext.resume();
-        }
+            audioContext.resume();
+        };
 
         setGain(updatedGain);
         setStereo(updatedStereo);
     }
 
-    const configVolume = (value:number) => {
-        if (!gain) return;
-        gain.gain.value = value;
-        setVolume(value);
+    const configVolume = (value: number) => {
+        if (gain) {
+            gain.gain.value = value;
+            setVolume(value);
+        }
     }
 
     const configMuted = () => {
-        if (!audio) return;
-        audio.muted = !muted;
-        setMuted(!muted);
+        if (audio) {
+            audio.muted = !muted;
+            setMuted(!muted);
+        }
     }
 
-    const configPanner = (value:number) => {
-        if (!stereo) return;
-        stereo.pan.value = value;
-        setPanner(value);
+    const configPanner = (value: number) => {
+        if (stereo) {
+            stereo.pan.value = value;
+            setPanner(value);
+        }
     }
 
     const configPlayPause = () => {
         if (playing) {
             pause();
-        }
-        else {
+        } else {
             play();
         }
         setPlaying(!playing);
     }
 
-    const play = ()=> {
-        if (!audio) return;
-        audio.play();
+    const play = () => {
+        if (audio) {
+            audio.play();
+        }
     }
 
     const pause = () => {
-        if (!audio) return;
-        audio.pause();
+        if (audio) {
+            audio.pause();
+        }
     }
 
     return (
-        <HomeContext.Provider value={
-            {
-                playing,
-                volume,
-                panner,
-                audioIndex,
-                currentTime,
-                totalTime,
-                muted,
-                configPlayPause,
-                configAudio,
-                configAudioIndex,
-                configVolume,
-                configPanner,
-                configCurrentTime,
-                configMuted,
-            }
-        }>
-          {children} 
+        <HomeContext.Provider value={{
+            playing,
+            volume,
+            panner,
+            audioIndex,
+            currentTime,
+            totalTime,
+            muted,
+            configPlayPause,
+            configAudio,
+            configAudioIndex,
+            configVolume,
+            configPanner,
+            configCurrentTime,
+            configMuted,
+        }}>
+            {children}
         </HomeContext.Provider>
     )
 }
--- src/app/dados/music.ts
+++ src/app/dados/music.ts
@@ -9,23 +9,23 @@ type Music = {
 export const musics: Music[] = [
     {
         name: "musica 01",
-        author: "ruan plabblo",
+        author: "unha pintada",
         description: "musica 01",
         urlAudio: "audios/audio1.mp3",
-        image: "imagens/image01.jpg"
+        image: "/imagens/image01.jpg"
     },
     {
         name: "musica 02",
-        author: "joao felepe",
+        author: "Japazin",
         description: "musica 02",
         urlAudio: "audios/audio2.mp3",
-        image: "imagens/image02.jpg"
+        image: "/imagens/image02.jpg"
     },
     {
         name: "musica 03",
-        author: "Antonio Caue",
+        author: "calcinha preta",
         description: "musica 03",
         urlAudio: "audios/audio3.mp3",
-        image: "imagens/image03.jpg"
+        image: "/imagens/image03.jpg"
     }
 ]
